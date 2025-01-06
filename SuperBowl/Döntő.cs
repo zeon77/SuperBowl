d16a6e72675f57485e4b2ae83ccf056469272dc6
zeon77 <2716313+zeon77@users.noreply.github.com>
2025-01-06 09:39:47 +0100

    3-4

---
+++ SuperBowl/Döntő.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBowl
+{
+    internal class Döntő
+    {
+        public string SSz { get; set; }
+        public string Dátum { get; set; }
+        public string Győztes { get; set; }
+        public string Eredmény { get; set; }
+        public string Vesztes { get; set; }
+        public string Helyszín { get; set; }
+        public string VárosÁllam { get; set; }
+        public int Nézőszám { get; set; }
+
+        //Consturctor
+        public Döntő(string sor)
+        {
+            string[] s = sor.Split(';');
+            SSz = s[0];
+            Dátum = s[1];
+            Győztes = s[2];
+            Eredmény = s[3];
+            Vesztes = s[4];
+            Helyszín = s[5];
+            VárosÁllam = s[6];
+            Nézőszám = int.Parse(s[7]);
+        }
+    }
+}
--- SuperBowl/Program.cs
+++ SuperBowl/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Beolvasás
+            List<Döntő> döntők = new();
+            foreach (var sor in File.ReadAllLines("SuperBowl.txt").Skip(1))
+            {
+                döntők.Add(new Döntő(sor));
+            }
+
+            Console.WriteLine($"4. feladat: Döntők száma: {döntők.Count}");
         }
     }
 }
